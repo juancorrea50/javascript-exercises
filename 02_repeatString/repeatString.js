e5b40eaee49bab4899caeed354ad51fe640222ae
Juan Correa <juancorrea50@yahoo.com>
2023-03-22 15:07:40 -0500

    Complete exercises until 04

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let newString='';
+    if(num <0){
+        return 'ERROR'
+    }
+    for(let x=0;x<num;x++){
+        newString += str;
+    }
+    return newString;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let newString = '';
+    for(x=0;x<str.length;x++){
+        newString += str.charAt(str.length-1-x);
+    }
+    return newString;
 };
 
 // Do not edit below this line
