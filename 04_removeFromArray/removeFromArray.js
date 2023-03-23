8ad55a4dbeb528f322c49edbf060439e6095304e
Juan Correa <juancorrea50@yahoo.com>
2023-03-23 03:25:00 -0500

    Pass first test of removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,21 @@
 const removeFromArray = function() {
+    //Create variables
+    const inputArray = arguments[0];
+    let newArray = [];
+
+    //Scans inputArray
+    for(let x=0;x<inputArray.length;x++){
+        //Find the number to remove
+        if(x == arguments[1]){
+            //Remove the number and output the new array into newArray
+            console.log(inputArray); 
+            inputArray.splice(x-1, 1);
+            newArray = inputArray;
+        }
+    }
+    //Prints and returns newArray
+    console.log(newArray);
+    return newArray;
 
 };
 
