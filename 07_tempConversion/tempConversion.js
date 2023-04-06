de4b3873f529714b40f2ca0a782fcde7b0af9a14
Juan Correa <juancorrea50@yahoo.com>
2023-04-06 16:40:26 -0500

    Complete up to 07 tempConversion
    Really easy simple mathematics to convert between C and F

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(input) {
+  //Use parameter as constant variable
+  let output =0;
+  output = ((input-32)*(5/9));
+  return parseFloat(output.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(input) {
+  let output = 0;
+  output = (input*(9/5)+32);
+  return parseFloat(output.toFixed(1));
 };
 
 // Do not edit below this line
