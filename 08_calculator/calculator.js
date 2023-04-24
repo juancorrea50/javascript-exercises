00039c8f78a763ed72bdc1f4dd6bedafa5ea6723
Juan Correa <juancorrea50@yahoo.com>
2023-04-24 10:26:43 -0500

    Complete up until multiply function
    Needed to look at solution to solve as solving multiplicity was
    lacking context for me. Solution included array.reduce which flew over
    my head. Will consider for other solutions to functions.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,16 +1,23 @@
-const add = function() {
-	
+const add = function(v1,v2) {
+    return v1+v2;
 };
 
-const subtract = function() {
-	
+const subtract = function(v1, v2) {
+    return v1-v2;
 };
 
-const sum = function() {
-	
+const sum = function([...input]) {
+  let sum=0;
+	input.forEach(val => sum += val);
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  return array.length
+    ? array.reduce((accumulator, nextItem) => accumulator * nextItem)
+    : 0;
+    //Ternary operator that says if there is an array length, array.reduce with an arrow function including the 
+    //first item marked accumulator and the second item marked nextItem then multiply them until the entire array is multiplied
 
 };
 
