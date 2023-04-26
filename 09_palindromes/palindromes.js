c099f35fe13a361357ff969c32360d9cd6ed224c
Juan Correa <juancorrea50@yahoo.com>
2023-04-25 19:16:15 -0500

    Complete palindromes with solution logic
    Referred and updated code to reflect solution logic

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,24 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    //take in a string copy all it's characters backwards and if the strings match return true if not return false
+    //:Solution USED: Flawed logic in previous code was comparing to input string and not the edited array of characters from the string.
+
+    const input = Array.from(string.toLowerCase().replace(/[^a-z]/g, ''));
+    const outputArray = [];
+
+    input.forEach(char => outputArray.unshift(char));
+
+    console.log(outputArray.join(''));
+
+    return outputArray.join('') == input.join('');
+    
+    /*:SOLUTION: Compared and contrasted with original source code and edited source code to follow logic :Solution is still more efficient:
+    //Takes the input string and converts it into a string that only contains lower case letters and no punctuation
+    const processedString = string.toLowerCase().replace(/[^a-z]/g, "");
+
+    //returns the truth or false value if the processed string converted into an array by split method, reversed in order by the reverse method, and joined without
+    //the commas carried over from the split method is equal to the processed string.
+    return processedString.split("").reverse().join("") == processedString;
+    */
 
 };
 
